4ea38ef6ad258817115efd6c6e8ed2e2a708e124
itahTamar <tamar.verber@gmail.com>
2025-10-24 17:23:11 +0300

    update Login screen till connection to DB

--- TableClient/src/components/users/login/Login.tsx
+++ TableClient/src/components/users/login/Login.tsx
@@ -12,12 +12,13 @@ const Login = () => {
   const navigate = useNavigate();
   const serverUrl = useContext(ServerContext);
   const getAllUserTables = useGetAllUserTables();
-  const tableContext = useContext(TablesContext); 
+  const tableContext = useContext(TablesContext);
   const { setUserEmail } = useContext(UserContext);
   const [password, setPassword] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [visible, setVisible] = useState(false);
   const [timeoutId, setTimeoutId] = useState<number | null>(null);
+  const [loginFlag, setLoginFlag] = useState(false);
 
   if (!tableContext) {
     throw new Error("TablesContext must be used within a TableProvider");
@@ -26,6 +27,7 @@ const Login = () => {
   const handleSubmitLogin = async (ev: React.FormEvent<HTMLFormElement>) => {
     try {
       ev.preventDefault();
+      setLoginFlag(true);
       console.log(
         "At handleSubmitLogin the email & password are:",
         email,
@@ -63,62 +65,68 @@ const Login = () => {
 
   return (
     <div>
-      <form className="relative top-24" onSubmit={handleSubmitLogin}>
-        <h1 className="text-4xl pb-5">Welcome</h1>
+      {loginFlag ? (
+        <div>Please wait while login.</div>
+      ) : (
+        <form className="relative top-24" onSubmit={handleSubmitLogin}>
+          <h1 className="text-4xl pb-5">Welcome</h1>
 
-        <div className="m-6">
-          <label className="text-2xl">Please enter your Email</label>
-          <div>
-            <input
-              className="border border-black m-2 rounded-2xl w-80 indent-4"
-              type="email"
-              name="email"
-              autoComplete="given-email"
-              value={email}
-              onInput={(ev) => setEmail((ev.target as HTMLInputElement).value)}
-            />
+          <div className="m-6">
+            <label className="text-2xl">Please enter your Email</label>
+            <div>
+              <input
+                className="border border-black m-2 rounded-2xl w-80 indent-4"
+                type="email"
+                name="email"
+                autoComplete="given-email"
+                value={email}
+                onInput={(ev) =>
+                  setEmail((ev.target as HTMLInputElement).value)
+                }
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="m-6">
-          <label className="text-2xl">Please enter your Password</label>
+          <div className="m-6">
+            <label className="text-2xl">Please enter your Password</label>
+            <div>
+              <input
+                className="border border-black m-2 rounded-2xl w-80 relative left-4 indent-4"
+                type={visible ? "text" : "password"}
+                name="password"
+                autoComplete="off"
+                value={password}
+                onInput={(ev) =>
+                  setPassword((ev.target as HTMLInputElement).value)
+                }
+              ></input>
+              <button
+                type="button"
+                className="emoji-button visible "
+                onClick={() => toggleVisibility()}
+              >
+                <span className="emoji">&#128065;</span>
+                {visible ? <span className="slash">/</span> : null}
+              </button>
+            </div>
+          </div>
+
           <div>
-            <input
-              className="border border-black m-2 rounded-2xl w-80 relative left-4 indent-4"
-              type={visible ? "text" : "password"}
-              name="password"
-              autoComplete="off"
-              value={password}
-              onInput={(ev) =>
-                setPassword((ev.target as HTMLInputElement).value)
-              }
-            ></input>
-            <button
-              type="button"
-              className="emoji-button visible "
-              onClick={() => toggleVisibility()}
+            <a
+              href="#"
+              onClick={() => navigate("/forgotPassword")}
+              className="text-gray-800"
             >
-              <span className="emoji">&#128065;</span>
-              {visible ? <span className="slash">/</span> : null}
-            </button>
+              {" "}
+              Forgot Password?
+            </a>
           </div>
-        </div>
 
-        <div>
-          <a
-            href="#"
-            onClick={() => navigate("/forgotPassword")}
-            className="text-gray-800"
-          >
-            {" "}
-            Forgot Password?
-          </a>
-        </div>
-
-        <button className="login text-xl" type="submit">
-          Log in
-        </button>
-      </form>
+          <button className="login text-xl" type="submit">
+            Log in
+          </button>
+        </form>
+      )}
     </div>
   );
 };
